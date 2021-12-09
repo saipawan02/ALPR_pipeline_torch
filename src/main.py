a22e4ce484858b7e895901f0179c1ac8b1bb0e71
PAWAN <saipawan000@gmail.com>
2021-12-09 18:58:29 +0530

    Add report

--- src/main.py
+++ src/main.py
@@ -1,20 +1,13 @@
-import cv2
-import math
-import numpy as np
 import time
 from sklearn.cluster import DBSCAN
 import torch
 import sys
 import os
-import yolov5
-import tensorflow as tf
-from PIL import Image
-import tensorflow_hub as hub
-# import easyocr
-from paddleocr import PaddleOCR,draw_ocr
+from paddleocr import PaddleOCR, draw_ocr
 import cv2
+import json
+import datetime
 import numpy as np
-import glob
 import re
 
 os.environ["TFHUB_DOWNLOAD_PROGRESS"] = "True"
@@ -22,107 +15,24 @@ os.environ["TFHUB_DOWNLOAD_PROGRESS"] = "True"
 from yolov5 import train, val, detect, export, YOLOv5
 
 
-# def preprocess_image(image_path):
-#     hr_image = tf.image.decode_image(tf.io.read_file(image_path))
-#     if hr_image.shape[-1] == 4:
-#         hr_image = hr_image[..., :-1]
-#     hr_size = (tf.convert_to_tensor(hr_image.shape[:-1]) // 4) * 4
-#     hr_image = tf.image.crop_to_bounding_box(hr_image, 0, 0, hr_size[0], hr_size[1])
-#     hr_image = tf.cast(hr_image, tf.float32)
-#     return tf.expand_dims(hr_image, 0)
-
-
-# def save_image(image, filename):
-#     if not isinstance(image, Image.Image):
-#         image = tf.clip_by_value(image, 0, 255)
-#         image = Image.fromarray(tf.cast(image, tf.uint8).numpy())
-#     image.save(filename)
-
-
-# def superResoluteFunc(maskedFileName, superResolution_model):
-#     hr_image = preprocess_image(maskedFileName)
-
-#     fake_image = superResolution_model(hr_image)
-#     fake_image = tf.squeeze(fake_image)
-
-#     # SRImgfileName = maskedFileName.split('.')[0]  # + "_Super_Resoluted"
-
-#     save_image(tf.squeeze(fake_image), filename=maskedFileName)
-
-#     return maskedFileName
-
-
 def ocrFunc(image_path, ocr):
     output = []
     st = ""
     avg = 0
-    dic = {'O': 0,'o':0,'A':4,'J':3,'l':1, 'L':1,'i':1,'I':1,'S':5, 'G':6,'H':8, 'D':0,'B':8,'q':9,'e':8,'b':6}
-    
+    dic = {'O': 0, 'o': 0, 'A': 4, 'J': 3, 'l': 1, 'L': 1, 'i': 1, 'I': 1, 'S': 5, 'G': 6, 'H': 8, 'D': 0, 'B': 8,
+           'q': 9, 'e': 8, 'b': 6}
     result = ocr.ocr(image_path, cls=True)
-    print(result,image_path)
     for line in result:
-      output.append(line[-1])
-    
-      st += line[-1][0] 
-      avg += line[-1][1] 
-    #   print(image_path, line[-1])
-    if len(result)!=0:
-      avg = avg/len(result)
+        output.append(line[-1])
+        st += line[-1][0]
+        avg += line[-1][1]
+    if len(result) != 0:
+        avg = avg / len(result)
 
     st = re.sub('[\W_]+', '', st)
-    if(len(st) < 9 or len(st) > 13):
-        # ss = st[0:-4]
-        # print(ss)   
-        # for i in st[-4:-1]:
-        #     a = i
-        #     if i in dic.keys():
-        #         a = str(dic[i])
-        #     ss+=a
+    if len(st) < 9 or len(st) > 13:
         st = ""
-    print(st)
-    return (st,avg) 
-
-
-def vehicleDetectionFunc(vech_model, filename, frame):
-    # declaring masked frame
-    maskedFrame = np.zeros(frame.shape, dtype=float)
-
-    vech_results = vech_model(filename)
-
-    # parse results
-    predictions = vech_results.pred[0]
-    boxes = predictions[:, :4].numpy()  # x1, x2, y1, y2
-    scores = predictions[:, 4]
-    categories = predictions[:, 5]
-
-    detected_names = []
-
-    for label in categories:
-        detected_names.append(labels_dic[int(label)])
-
-    # creating a dict for the vechicle results
-    vech_dic = {}
-
-    vech_dic['predictions'] = predictions
-    vech_dic['boxes'] = boxes
-    vech_dic['scores'] = scores
-    vech_dic['categories'] = categories
-    vech_dic['categories_names'] = detected_names
-
-    # print('vehicle', vech_dic['categories_names'])
-
-    # initializing masked frame
-    for box in boxes:
-        (x1, y1, x2, y2) = (round(box[0]),
-                            round(box[1]),
-                            round(box[2]),
-                            round(box[3]))
-        maskedFrame[y1:y2, x1:x2, :] = frame[y1:y2, x1:x2, :]
-
-    # save results into "vehicleResults/" folder
-    # vech_results.save(save_dir='vehicleResults/')
-
-    return (maskedFrame, vech_dic)
+    return st, avg
 
 
 def licenseDetectionFunc(license_model, maskedFrame):
@@ -130,7 +40,7 @@ def licenseDetectionFunc(license_model, maskedFrame):
 
     # parse results
     predictions = license_results.pred[0]
-    boxes = predictions[:, :4].cpu().numpy() # x1, x2, y1, y2
+    boxes = predictions[:, :4].cpu().numpy()  # x1, x2, y1, y2
     scores = predictions[:, 4]
     categories = predictions[:, 5]
 
@@ -156,78 +66,51 @@ def licenseDetectionFunc(license_model, maskedFrame):
 
     return license_dic
 
+try:
+    for f in os.listdir('../numberplates'):
+        os.remove(os.path.join('../numberplates', f))
+    os.remove("../report/report.json")
+except FileNotFoundError:
+    print("No files to delete")
 
-# vech_model = torch.hub.load('ultralytics/yolov5', 'yolov5s', device='cpu')  # load on CPU
-# vech_model.classes = [2, 3, 5, 7]  # 'car', 'motorcycle', 'bus', 'truck'
-
-# set model params for License detection
 model_path = "../yolo/torch/best.pt"
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-# init yolov5 model for license detection
 license_model = YOLOv5(model_path, device)
 
-
-# Vehicle labels
-labels_dic = {2: 'car', 3: 'motorcycle', 5: 'bus', 7: 'truck'}
-
-# start = time.time()
-# main code goes here
 cap = cv2.VideoCapture("../data/video 3.mp4")
 frameRate = cap.get(5)  # frame rate
 frameCount = 1
 
 object_detector = cv2.createBackgroundSubtractorMOG2(history=200, varThreshold=100)
 
-# Super Resolution model weights
-# SAVED_MODEL_PATH = "https://tfhub.dev/captain-pool/esrgan-tf2/1"
+ocr = PaddleOCR(use_angle_cls=True, lang='en')
 
-# superResolution_model = hub.load(SAVED_MODEL_PATH)
-
-# easyocr weights
-# reader = easyocr.Reader(['en'])
-
-ocr = PaddleOCR(use_angle_cls=True, lang='en') 
-
-
-out = cv2.VideoWriter('output_video.avi',cv2.VideoWriter_fourcc(*'DIVX'), 60, (500,500))
-
+report = {
+    'result': []
+}
 
 while cap.isOpened():
-
     frameId = cap.get(1)  # current frame number
     ret, frame = cap.read()
-    if(frame is None):
+
+    if not ret:
         break
     height, width, _ = frame.shape
 
     # frame = frame[height // 2:, ::]
 
-    if not ret:
-        break
     if frameId % 3 == 0:
 
         start = time.time()
-        print(frameCount)
-
-        # filename = '../frames/' + str(int(frameCount)) + ".png"
-        # cv2.imwrite(filename, frame)
-        # maskedFileName = '../maskedFrames/' + str(int(frameCount)) + ".png"
-
         mask = object_detector.apply(frame)
         _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_OTSU)
-        # mask = cv2.GaussianBlur(mask, (11, 11), 4)
-        # kernel = np.ones((5, 5), np.uint8)
-        # mask = cv2.dilate(mask, kernel, iterations=10)
         contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         rects = []
-
-        # Bool array indicating which initial bounding rect has
-        # already been used
         rectsUsed = []
         rect_center = []
-        # Just initialize bounding rects and set all bools to false
+
         for cnt in contours:
             if cv2.contourArea(cnt) > 1500:
                 x, y, w, h = cv2.boundingRect(cnt)
@@ -271,10 +154,6 @@ while cap.isOpened():
                                 round(box[3]))
             maskedFrame[y1:y2, x1:x2, :] = frame[y1:y2, x1:x2, :]
 
-        # cv2.imshow("Mask", maskedFrame)
-        # Saving masked Frame
-        # cv2.imwrite(maskedFileName, maskedFrame)
-
         license_dic = licenseDetectionFunc(license_model, maskedFrame)
 
         recognizedTextList = []
@@ -282,44 +161,47 @@ while cap.isOpened():
         ll = 1
         for box in license_dic['boxes']:
             (x1, y1, x2, y2) = (box[0], box[1], box[2], box[3])
+            if (x2 - x1) * (y2 - y1) > 50000:
+                continue
 
-
-
-            # Image.fromarray(maskedFrame[y1:y2, x1:x2, :])
-
-            
             detectedLicenseFileName = '../numberplates/' + str(int(frameCount)) + str(ll) + ".png"
 
-            cv2.imwrite(detectedLicenseFileName,
-                        maskedFrame[y1:y2, x1:x2, :])
+            cv2.imwrite(detectedLicenseFileName, maskedFrame[y1:y2, x1:x2, :])
+            text_disp, conf = ocrFunc(detectedLicenseFileName, ocr)
 
-            # detectedLicenseFileName = superResoluteFunc(detectedLicenseFileName, superResolution_model)
-            text_disp,conf = ocrFunc(detectedLicenseFileName, ocr)
-            # print(text)
-            if(len(text_disp)!=0):
+            if len(text_disp) != 0:
+                print(text_disp, detectedLicenseFileName)
+
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
-                cv2.putText(frame,text_disp + " " + str(round(conf, 3)), (x1, y1), cv2.FONT_HERSHEY_PLAIN, 2, (0,0,255), 3)
+                cv2.putText(frame, text_disp + " " + str(round(conf, 3)), (x1, y1), cv2.FONT_HERSHEY_PLAIN, 2,
+                            (0, 0, 255), 3)
+
+                report['result'].append({
+                    'frame_number': frameCount,
+                    'time_stamp':str(datetime.datetime.now()),
+                    'license_plate': detectedLicenseFileName,
+                    'text': text_disp,
+                    'confidence': str(round(conf, 3))
+                })
 
             ll += 1
 
-        # print('license', license_dic['categories'])
-
-        # print(recognizedTextList)
         elapsed_time = time.time() - start
         fps = 1 / elapsed_time
 
         # cv2.putText(frame, "FPS: " + str(round(fps, 2)), (10, 50), cv2.FONT_HERSHEY_PLAIN, 4, (0, 255, 0), 3)
-
         cv2.namedWindow('Frame', cv2.WINDOW_NORMAL)
         cv2.imshow("Frame", frame)
 
-        out.write(frame)
+        # cv2.imwrite('../frames/currentFrame.jpg', frame)
 
         key = cv2.waitKey(30)
         if key == 27:
             break
-        frameCount += 1  # Incrementing frameCount value
+        frameCount += 1
 
 cap.release()
-out.release()
+json_object = json.dumps(report, indent=4)
+with open("../report/report.json", "w") as outfile:
+    outfile.write(json_object)
 print("Done!")
